bf6db221f7c16dd6d4667ab018a96852ef4e3827
Jackson Kisling <jackson@Minnesota-Martian.com>
2015-05-27 12:51:30 -0500

    cleaned up formatting

--- Shield.java
+++ Shield.java
@@ -17,48 +17,44 @@ public class Shield extends Brawler implements Treasure {
 		playground = new int[1];
 	}
 	
-	public int getFloor() {return this.floor;}
-	public int getColumn() {return this.column;}
-	public int getRow() {return this.row;}
-	public int getLevel() {return this.level;}
-	public int getSwordBonus() {return this.swordBonus;}
-	public int getShieldBonus() {return this.shieldBonus;}
-	public int getClericDefenseBonus() {return this.clericDefenseBonus;}
-	public int getPossession() {return this.possession;}
-	public int getPieceID() {return this.pieceID;}
-	public int[] getPlayground() {return this.playground;}
-	public char getSuit() {return this.suit;}
-	public boolean isOnBoard() {return this.onBoard;}
-	public boolean isMoveable() {return false;}
-	public boolean isUsedLast() {return this.usedLast;}
-	public boolean isRed() {return this.isRed;}
-	public boolean isBlue() {return this.isBlue;}
-	public boolean isPoweredUp() {return false;}
-	public boolean isHandsFull() {return false;}
-	public boolean isGlorified() {return false;}
-	public String getLetterName() {return this.letterName;}
+	public int getFloor() 			{ return this.floor; }
+	public int getColumn() 			{ return this.column; }
+	public int getRow() 			{ return this.row; }
+	public int getLevel() 			{ return this.level; }
+	public int getSwordBonus() 		{ return this.swordBonus; }
+	public int getShieldBonus() 		{ return this.shieldBonus; }
+	public int getClericDefenseBonus() 	{ return this.clericDefenseBonus; }
+	public int getPossession() 		{ return this.possession; }
+	public int getPieceID() 		{ return this.pieceID; }
+	public int[] getPlayground() 		{ return this.playground; }
+	public char getSuit() 			{ return this.suit; }
+	public boolean isOnBoard() 		{ return this.onBoard; }
+	public boolean isMoveable() 		{ return true; }
+	public boolean isUsedLast() 		{ return this.usedLast; }
+	public boolean isRed() 			{ return this.isRed; }
+	public boolean isBlue() 		{ return this.isBlue; }
+	public boolean isPoweredUp() 		{ return this.poweredUp; }
+	public boolean isHandsFull() 		{ return this.handsFull; }
+	public boolean isGlorified() 		{ return this.glorified; }
+	public String getLetterName() 		{ return this.letterName; }
 	
-	public void setFloor(int x) {this.floor = x;}
-	public void setColumn(int x) {this.column = x;}
-	public void setRow(int x) {this.row = x;}
-	public void setLevel(int x) {this.level = x;} 
-	public void setSwordBonus(int x) {this.swordBonus = x;} 
-	public void setShieldBonus(int x) {this.shieldBonus = x;} 
-	public void setClericDefenseBonus(int x) {this.clericDefenseBonus = x;} 
-	public void setPossession(int x) {this.possession = x;}
-	public void setPieceID(int x) {this.pieceID = x;}
-	public void setPlayground(int[] x) {this.playground = x;}
-	public void setOnBoard(boolean x) {this.onBoard = x;}
-	public void setUsedLast(boolean x) {this.usedLast = x;}
-	public void setPoweredUp(boolean x) {return;}
-	public void setHandsFull(boolean x) {return;}
-	public void setGlorified(boolean x) {return;}
+	public void setFloor(int x) 		{ this.floor = x; }
+	public void setColumn(int x) 		{ this.column = x; }
+	public void setRow(int x) 		{ this.row = x; }
+	public void setLevel(int x) 		{ this.level = x; } 
+	public void setSwordBonus(int x) 	{ this.swordBonus = x; } 
+	public void setShieldBonus(int x) 	{ this.shieldBonus = x; } 
+	public void setClericDefenseBonus(int x){ this.clericDefenseBonus = x; } 
+	public void setPossession(int x) 	{ this.possession = x; } 
+	public void setPieceID(int x) 		{ this.pieceID = x; }
+	public void setPlayground(int[] x) 	{ this.playground = x; }
+	public void setOnBoard(boolean x) 	{ this.onBoard = x; }
+	public void setUsedLast(boolean x) 	{ this.usedLast = x; }
+	public void setPoweredUp(boolean x) 	{ this.poweredUp = x; }
+	public void setHandsFull(boolean x) 	{ this.handsFull = x; }
+	public void setGlorified(boolean x) 	{ this.glorified = x; }
 	
-	public void getsBumped() {
-		this.remove();
-	}
+	public boolean canAttack(Brawler victim) { return false; }
 	
-	public boolean canAttack(Brawler victim) {
-		return false;
-	}
+	public void getsBumped() { this.remove(); }
 } // end of Shield class
