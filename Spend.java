edd28e4e3fdb38652acb6c0dd656299753f22ab2
Jackson Kisling <jackson@Minnesota-Martian.com>
2015-06-17 15:55:59 -0500

    Major fix 6/17

--- Spend.java
+++ Spend.java
@@ -7,29 +7,19 @@ public class Spend extends GridBrawl {
 	
 	public Spend() {}
 	
+	// relies on standard set of Brawlers
 	public GameData execute(GameData crt) {
-		Sword sword = new Sword();
-		Shield shield = new Shield();
-		Ring ring = new Ring();
-		mephisto = new Diamond();
-		for (int a = 0; a < crt.getAllBrawlers().length; a++) {
-			Brawler loot = crt.getBrawler(a);
-			if (loot instanceof Diamond) mephisto = crt.getBrawler(a);
-			if (mephisto.getPieceID() == loot.getPossession()) faust = crt.getBrawler(a);
-			if (loot instanceof Sword) sword = (Sword) crt.getBrawler(a);
-			if (loot instanceof Shield) shield = (Shield) crt.getBrawler(a);
-			if (loot instanceof Ring) ring = (Ring) crt.getBrawler(a);	
-		}
-		boolean bonusFunction = (faust instanceof Rogue);
+		Brawler sword = crt.getBrawler(20);
+		Brawler shield = crt.getBrawler(21);
+		Brawler ring = crt.getBrawler(22);
+		mephisto = crt.getBrawler(23);
+		for (int f = 0; f < 8; f++) if (crt.getBrawler(f).getPossession() == 23) faust = crt.getBrawler(f);
 		int[] bribeList = buildTargetList(crt, faust);
-		boolean options1[] = new boolean[3];
+		boolean options1[] = new boolean[2]; // options1[0] is for bribe, options1[1] is for purchase
 		boolean confirm = false;
 		
-		if (bribeList.length >= 1) { // There is at least one brawler available to bribe
-			options1[0] = true;
-			for (int m = 0; m < bribeList.length; m++) if (crt.getBrawler(bribeList[m]) instanceof Merchant) options1[1] = true;
-		}
-		options1[2] = bonusFunction;
+		if ((faust instanceof Rogue) && bribeList.length >= 1) options1[0] = true; // There is at least one brawler available to bribe
+		if (faust.getFloor() == crt.getBrawler(19).getFloor()) options1[1] = true; // faust shares floor with merchant	
 		
 		interact.tellPlayer(5, 1, false); // "Please select from among the following options:"
 		interact.tellPlayer(interact.reportOnSpendOptions1(options1), true);
@@ -48,8 +38,8 @@ public class Spend extends GridBrawl {
 				crt.setSuccessfulTurn(false);
 				return crt;
 			}
-			Brawler bribed = crt.getBrawler(bribeList[opt2]);
-			interact.tellPlayer(interact.msgBuild(crt, 32, bribed.getPieceID()), false); // "You have selected to bribe the " + BRWLRS[bribed] + " into moving to a new space."
+			int bribed = bribeList[opt2];
+			interact.tellPlayer(interact.msgBuild(crt, 32, bribed), false); // "You have selected to bribe the " + BRWLRS[bribed] + " into moving to a new space."
 			interact.tellPlayer(8,  1, true); // "Enter the 3-digit number of any unoccupied space: "
 			int[] nwSpc = interact.getUnoccupiedSpaceNumber(crt);
 			if (nwSpc[0] == -1) {
@@ -57,20 +47,27 @@ public class Spend extends GridBrawl {
 				return crt;
 			}
 			int newSpace = nwSpc[0] * 100 + nwSpc[1] * 10 + nwSpc[2];
-			String result = "You have selected to use your" + BRWLRS[faust.getPieceID()] + " to bribe the " + BRWLRS[bribed.getPieceID()]+ " to space #" + newSpace + ".";
+			String result = "You have selected to use your" + BRWLRS[faust.getPieceID()] + " to bribe the " + BRWLRS[bribed]+ " to space #" + newSpace + ".";
 			interact.tellPlayer(result, false);
 			confirm = interact.confirm();
 			if (!confirm) {
 				crt.setSuccessfulTurn(false);
 				return crt;
 			}
+			Brawler brbd = crt.getBrawler(bribed);
+			crt.getLocation(brbd.getFloor(), brbd.getColumn(), brbd.getRow()).setOccupied(false);
+			crt.getLocation(brbd.getFloor(), brbd.getColumn(), brbd.getRow()).setOccupiedBy(-1);
+			dispossess(crt, mephisto.getPieceID());
+			crt.getBrawler(bribed).setFloor(nwSpc[0]);
+			crt.getBrawler(bribed).setColumn(nwSpc[1]);
+			crt.getBrawler(bribed).setRow(nwSpc[2]);
+			crt.getLocation(newSpace).setOccupied(true);
+			crt.getLocation(newSpace).setOccupiedBy(bribed);
+			crt.updateGameRecord("Bribe", BRWLRS[faust.getPieceID()], newSpace, BRWLRS[bribed]);
+			
 			interact.space();
 			interact.tellPlayer(8,  2, false); // "The Diamond has been spent, your bribe was successful."
 			interact.space();
-			dispossess(crt, mephisto.getPieceID());
-			crt.getBrawler(bribed.getPieceID()).setFloor(nwSpc[0]);
-			crt.getBrawler(bribed.getPieceID()).setColumn(nwSpc[1]);
-			crt.getBrawler(bribed.getPieceID()).setRow(nwSpc[2]);
 			break;
 		case 2: // purchase
 			Brawler merchandise = new Sword();
@@ -87,48 +84,10 @@ public class Spend extends GridBrawl {
 				crt.setSuccessfulTurn(false);
 				return crt;
 			}
-			for (int bb = 0; bb < crt.getAllBrawlers().length; bb++) if (merchandise.getPieceID() == crt.getBrawler(bb).getPossession()) dispossess(crt, merchandise.getPieceID());
-			crt.getBrawler(faust.getPieceID()).setPossession(merchandise.getPieceID());
-			crt.getBrawler(faust.getPieceID()).setHandsFull(true);
-			crt.getBrawler(merchandise.getPieceID()).setFloor(faust.getFloor());
-			crt.getBrawler(merchandise.getPieceID()).setColumn(faust.getColumn());
-			crt.getBrawler(merchandise.getPieceID()).setRow(faust.getRow());
+			crt = dispossess(crt, merchandise.getPieceID());
+			crt.takePossession(faust.getPieceID(), merchandise.getPieceID());
+			crt.updateGameRecord("Purchase", BRWLRS[faust.getPieceID()], 0, BRWLRS[merchandise.getPieceID()]);
 			break;
-		case 3: // invest
-			interact.tellPlayer(8, 4, false); // "Your Rogue may 'invest' the Diamond, and in doing so will take possession of any one Treasure on its board level."
-			boolean[] opts3 = {(sword.getFloor() == faust.getFloor()), (shield.getFloor() == faust.getFloor()), (ring.getFloor() == faust.getFloor())};
-			if (!opts3[0] && !opts3[1] && !opts3[2]) {
-				interact.tellPlayer(interact.msgBuild(crt, 34, faust.getPieceID()), false);
-				interact.space();
-				crt.setSuccessfulTurn(false);
-				return crt;
-			}
-			interact.tellPlayer(interact.reportOnSpendOptions3(opts3), true);
-			int yieldSelect = interact.getSpendSelection4(opts3);
-			if (yieldSelect == -1) {
-				crt.setSuccessfulTurn(false);
-				return crt;
-			}
-			Brawler yield = new Diamond();
-			if (yieldSelect == 1) yield = sword;
-			if (yieldSelect == 2) yield = shield;
-			if (yieldSelect == 3) yield = ring;
-			result = "You have selected to invest the Diamond, which will yield the " + BRWLRS[yield.getPieceID()] + " to the " + BRWLRS[faust.getPieceID()];
-			interact.tellPlayer(result, false);
-			confirm = interact.confirm();
-			if (!confirm) {
-				crt.setSuccessfulTurn(false);
-				return crt;
-			}
-			dispossess(crt, yield.getPieceID());
-			crt.getBrawler(faust.getPieceID()).setPossession(yield.getPieceID());
-			crt.getBrawler(faust.getPieceID()).setHandsFull(true);
-			crt.getBrawler(yield.getPieceID()).setFloor(faust.getFloor());
-			crt.getBrawler(yield.getPieceID()).setColumn(faust.getColumn());
-			crt.getBrawler(yield.getPieceID()).setRow(faust.getRow());
-			interact.space();
-			interact.tellPlayer(8, 5, false); // "The Diamond has been spent, your investment was fruitful.";
-			interact.space();
 		}
 		crt.setSuccessfulTurn(true);
 		if (crt.isRedsTurn()) crt.setRedCanPlace(true);
@@ -139,13 +98,19 @@ public class Spend extends GridBrawl {
 	} // end of execute()
 	
 	// whoever has the treasure in question gives it up.
-	private static void dispossess(GameData crt, int loot) {
+	private static GameData dispossess(GameData crt, int loot) {
 		int formerOwner = -1;
 		for (int d = 0; d < crt.getAllBrawlers().length; d++) if (loot == crt.getBrawler(d).getPossession()) formerOwner = d;	
-		if (formerOwner != -1) {
+		if (formerOwner != -1) { // loot is currently owned
 			crt.getBrawler(formerOwner).setHandsFull(false);
 				crt.getBrawler(formerOwner).setPossession(-1);
-		}	
+		}
+		else { // loot is loose on the board
+			crt.getLocation(crt.getBrawler(loot).getLoc()).setOccupied(false);
+			crt.getLocation(crt.getBrawler(loot).getLoc()).setOccupiedBy(-1);
+		}
+		crt.getBrawler(loot).remove();
+		return crt;
 	}	
 	
 } // end of Spend class
