08e1ee8804c1421b161dc511783a937eab7f20cb
Jackson Kisling <jackson@Minnesota-Martian.com>
2015-07-26 19:43:00 -0500

    fixed possession bug

--- Abyss.java
+++ Abyss.java
@@ -3,7 +3,8 @@ package rpgGridBrawl;
 public class Abyss extends Brawler implements Nemesis {
 	private int floor, column, row, level, pieceID;
 	private boolean onBoard, usedLast;
-	private final boolean isRed, isBlue;
+	private final boolean isRed, isBlue, handsFull;
+	private final int possession;
 	private final String letterName;
 	private final char suit;
 	
@@ -17,6 +18,7 @@ public class Abyss extends Brawler implements Nemesis {
 		level = 4;
 		poweredUp = false;
 		handsFull = false;
+		possession = -1;
 		glorified = false;
 	}
 	
@@ -47,12 +49,12 @@ public class Abyss extends Brawler implements Nemesis {
 	public void setSwordBonus(int x) 		{ this.swordBonus = x; } 
 	public void setShieldBonus(int x) 		{ this.shieldBonus = x; } 
 	public void setClericDefenseBonus(int x){ this.clericDefenseBonus = x; } 
-	public void setPossession(int x) 		{ this.possession = x; } 
+	public void setPossession(int x) 		{ return; } 
 	public void setPieceID(int x) 			{ this.pieceID = x; }
 	public void setOnBoard(boolean x) 		{ this.onBoard = x; }
 	public void setUsedLast(boolean x) 		{ this.usedLast = x; }
 	public void setPoweredUp(boolean x) 	{ this.poweredUp = x; }
-	public void setHandsFull(boolean x) 	{ this.handsFull = x; }
+	public void setHandsFull(boolean x) 	{ return; }
 	public void setGlorified(boolean x) 	{ this.glorified = x; }
 	
 	public boolean canAttack(Brawler victim) {
